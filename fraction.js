6841979d0c7975dc48a0a4236379c84a6a893573
luffy2220 <119392648+luffy2220@users.noreply.github.com>
2023-04-20 11:59:12 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,11 @@
-print("helloworld")
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+  divide(other) {
+    const newNumerator = this.numerator * other.denominator;
+    const newDenominator = this.denominator * other.numerator;
+    return new Fraction(newNumerator, newDenominator);
+  }
+}
